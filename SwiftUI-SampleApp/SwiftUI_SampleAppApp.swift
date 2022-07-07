0f39ec7ff7b95cf3664cbbbf0181526cf714b72c
wataru-taniuchi <wataru.taniuchi@asia-quest.jp>
2022-07-07 18:52:07 +0900

    initial commit

---
+++ SwiftUI-SampleApp/SwiftUI_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SampleAppApp.swift
+//  SwiftUI-SampleApp
+//
+//  Created by Taniuchi Wataru on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
